328a6799e83e2219cde062b7b6eeb6e01b70edbc
jmb05 <53855936+jmb05@users.noreply.github.com>
2020-10-03 11:55:11 +0200

    Fixed Server crash: LoadingFailedException

--- src/main/java/net/jmb19905/medievalworlds/item/armor/KnightArmorHelmetItem.java
+++ src/main/java/net/jmb19905/medievalworlds/item/armor/KnightArmorHelmetItem.java
@@ -9,6 +9,8 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
@@ -23,7 +25,7 @@ public class KnightArmorHelmetItem extends ArmorItem {
         this.material = material;
     }
 
-    @SuppressWarnings("unchecked")
+    @OnlyIn(Dist.CLIENT)
     @Nullable
     @Override
     public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
